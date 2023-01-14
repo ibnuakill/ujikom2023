181d8db9ed574eae9e7d1dd37900979304abe44d
Akiru <90877602+hyugoakiru@users.noreply.github.com>
2023-01-15 01:23:59 +0700

    Add files via upload

---
+++ ujikom2023/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    session_start();
+
+    if(!empty($_SESSION['nik'])){
+
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    tes
+
+</body>
+</html>
+
+<?php
+    }else{
+        header('location:login.php');
+    }
+    ?>
